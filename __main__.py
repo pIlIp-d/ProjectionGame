b45959f654e1e34acc7df56cc22f6b919e3dbaf9
pilip-d <philipdell@gmx.de>
2023-12-27 21:37:20 +0100

    added __main__.py

---
+++ __main__.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+from threading import Thread
+
+from src.controller.SetupController import SetupController
+from src.game.player_controller.MovenetHumanPoseEstimator import MovenetHumanPoseEstimator
+from src.models.Model import Model
+
+# todo make it properly resizeable
+
+# todo send width and height to game (from View) when it changes and in the beginning
+#   width = self._active_frame.winfo_width()
+#   height = self._active_frame.winfo_height()
+
+# todo make _update so that it doesnt need to check any conditions and add delta_time directly into it as parameter
+
+initable_classes = [
+    MovenetHumanPoseEstimator
+]
+
+
+def main():
+    root = tk.Tk()
+
+    model = Model()
+    SetupController(model, root)
+
+    def init():
+        for c in initable_classes:
+            c.init()
+
+    # todo find a better way, this one produces lag
+    Thread(target=init).start()
+
+    # app = CameraSelectView(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
